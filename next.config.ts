ebb0a2b7924ae7d5baf06e28dbb12520f5be9f21
Andrej Karamatic <andrej.karamatic20@gmail.com>
2025-10-15 11:36:39 +0200

    static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+const repo = "sportsync-web"; // npr. 'sportsync-web'
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // ključna stavka za statički export
+  images: { unoptimized: true }, // jer nema Image Optimization na GH Pages
+  basePath: isProd ? `/${repo}` : "", // za Project Pages (username.github.io/repo)
+  assetPrefix: isProd ? `/${repo}/` : "", // da asseti rade u pod-stazi
+  // trailingSlash: true, // uključi ako koristiš relative linkove koji očekuju završni /
 };
 
 export default nextConfig;
